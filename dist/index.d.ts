48a04a7210179e5acdf6deaaedf86d61170b696d
Ali <mr.ali.serjik@gmail.com>
2023-02-28 16:59:50 +0330

    remove redundant file

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,12 @@
-import { TableContextProvider, useTableContext } from './context';
-import { getPropByString } from './helpers/getPropByString';
-import type { TableColumnType, TableSortOrder, TableProps, TableRecord } from './types';
-export { TableContextProvider, useTableContext, getPropByString, type TableColumnType, type TableSortOrder, type TableProps, type TableRecord, };
+import { TableContextProvider, useTableContext } from './context'
+import { getPropByString } from './helpers/getPropByString'
+import type { TableColumnType, TableSortOrder, TableProps, TableRecord } from './types'
+export {
+  TableContextProvider,
+  useTableContext,
+  getPropByString,
+  type TableColumnType,
+  type TableSortOrder,
+  type TableProps,
+  type TableRecord,
+}
