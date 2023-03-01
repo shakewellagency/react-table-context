4204347bf55e0b14d33643eadb7829258c296383
Ali <mr.ali.serjik@gmail.com>
2023-03-01 16:04:45 +0330

    improve render actions

--- dist/store.d.ts
+++ dist/store.d.ts
@@ -1,4 +1,7 @@
-import { TableRecord, TableState } from './types';
-import { TableAction } from './action';
-export declare const initialState: TableState;
-export declare const createTableReducer: <T extends TableRecord = TableRecord>() => (state: TableState<T>, action: TableAction) => TableState<T>;
+import { TableRecord, TableState } from './types'
+import { TableAction } from './action'
+export declare const initialState: TableState
+export declare const createTableReducer: <T extends TableRecord = TableRecord>() => (
+  state: TableState<T>,
+  action: TableAction,
+) => TableState<T>
